74f2e162467ab69528b25a5015272287bcfe4edd
14EC002 <govindhpavi@gmail.com>
2017-08-11 10:08:56 +0530

    Create working.c

---
+++ working.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+scanf("%c",&a);
+switch(a)
+{
+case sunday:printf("holliday");break;
+default:printf("working day");break;
+}
+getch();
+}
